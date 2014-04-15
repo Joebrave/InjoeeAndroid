920abf93f1af6ba5d8d5cae6f198c4be80b05c65
beethoven5000 <beethoven5000@gmail.com>
2014-04-15 23:07:51 +0800

    for the sake of upgrading

--- src/com/injoee/util/Constant.java
+++ src/com/injoee/util/Constant.java
@@ -2,6 +2,6 @@ package com.injoee.util;
 
 public class Constant {
 	
-	public final static String SERVER_URL_PREFIX = "http://192.168.0.107/games/";
+	public final static String SERVER_URL_PREFIX = "http://192.168.0.103/games/";
 
 }
