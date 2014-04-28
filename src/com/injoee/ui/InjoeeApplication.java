b6a3190f1190d786b81b37fe506a64b75d152ac2
beethoven5000 <beethoven5000@gmail.com>
2014-04-29 00:10:00 +0800

    for 友盟的push通知

---
+++ src/com/injoee/ui/InjoeeApplication.java
@@ -0,0 +1,56 @@
+package com.injoee.ui;
+
+import com.umeng.message.PushAgent;
+import com.umeng.message.UTrack;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+
+public class InjoeeApplication extends Application {
+
+	private PushAgent mPushAgent;
+	private String packageName = "";
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		mPushAgent = mPushAgent = PushAgent.getInstance(this);
+		
+		UmengMessageHandler messageHandler = new UmengMessageHandler(){
+			@Override
+			public void dealWithCustomMessage(final Context context, final UMessage msg) {
+				new Handler(getMainLooper()).post(new Runnable() {
+					
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
+						packageName = msg.custom;
+						
+						
+					}
+				});
+			}
+		};
+		
+		mPushAgent.setMessageHandler(messageHandler);
+		
+		UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler(){
+			@Override
+			public void dealWithCustomAction(Context context, UMessage msg) {
+				
+				Intent intent = new Intent(context, GameDetail.class);
+				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				packageName = msg.custom;
+				intent.putExtra("package_name", packageName);
+				startActivity(intent);
+			}
+		};
+		mPushAgent.setNotificationClickHandler(notificationClickHandler);
+	}
+}
